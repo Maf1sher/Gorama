71ce42073da3b86f6e4219a05490b22b2f4487b8
Maf1sher <kubas307@gmail.com>
2025-09-03 15:36:45 +0200

    player animation improvements

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,11 +3,12 @@ extends CharacterBody2D
 
 @export var stats: Stats
 
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var animated_sprite = $AnimationPlayer
 @onready var health_bar = $ProgressBar
-@onready var weapon = $Sword
+@onready var weapon = $AnimatedSprite2D/Sword
 
 var input
+var current_look_dir = "right"
 
 func _ready() -> void:
 	health_bar.max_value = stats.maximum_hp
@@ -16,11 +17,15 @@ func _physics_process(delta: float) -> void:
 	input = Input.get_vector("left", "right", "up", "down")
 	velocity = input * stats.movement_speed
 		
-	if input.x != 0:
-		animated_sprite.flip_h = input.x > 0
+	if input.x > 0:
+		current_look_dir = "right"
+	elif input.x < 0:
+		current_look_dir = "left"
 		
-	if velocity:
-		animated_sprite.play("walk")
+	if velocity and current_look_dir == "right":
+		animated_sprite.play("walk_right")
+	elif velocity and current_look_dir == "left":
+		animated_sprite.play("walk_left")
 	else:
 		animated_sprite.play("idle")
 	
