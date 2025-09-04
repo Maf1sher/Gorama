b9eb38a7d26ba76e7e9754121aaf93ed5bda7863
Maf1sher <kubas307@gmail.com>
2025-09-04 02:41:20 +0200

    player death

--- scripts/enemy_generator.gd
+++ scripts/enemy_generator.gd
@@ -4,7 +4,7 @@ extends Node
 @onready var orc: PackedScene = preload("res://scenes/Orc.tscn")
 
 func _ready() -> void:
-	for i in 30:
+	for i in 1:
 		await get_tree().create_timer(1).timeout
 		var orc = orc.instantiate()
 		orc.position = Vector2(randi_range(-100,100),randi_range(-100,100))
--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,7 @@ extends CharacterBody2D
 
 var input
 var current_look_dir = "right"
+var alive = true
 
 func _ready() -> void:
 	health_bar.max_value = stats.maximum_hp
@@ -16,28 +17,40 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	input = Input.get_vector("left", "right", "up", "down")
 	velocity = input * stats.movement_speed
+	
+	if !alive:
+		velocity = Vector2.ZERO
 		
 	if input.x > 0:
 		current_look_dir = "right"
 	elif input.x < 0:
 		current_look_dir = "left"
 		
-	if velocity and current_look_dir == "right":
-		animated_sprite.play("walk_right")
-	elif velocity and current_look_dir == "left":
-		animated_sprite.play("walk_left")
-	else:
-		animated_sprite.play("idle")
-	
+	animation()
 	move_and_slide()
 	
 	if(Input.is_action_pressed("attack")):
 		weapon.play_attack_animation()
 
+func animation():
+	if !alive:
+		animated_sprite.play("death")
+	elif velocity and current_look_dir == "right":
+		animated_sprite.play("walk_right")
+	elif velocity and current_look_dir == "left":
+		animated_sprite.play("walk_left")
+	else:
+		animated_sprite.play("idle")
+
 func take_hit(damage: int) -> void:
 	var taken_hp = stats.take_hit(damage)
 	health_bar.value = stats.hp
 
-
 func _on_hurt_box_received_damage(damage: int) -> void:
 	health_bar.value = stats.hp
+	if stats.hp <= 0:
+		alive = false
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "death":
+		get_tree().change_scene_to_file("res://scenes/Menu.tscn")
