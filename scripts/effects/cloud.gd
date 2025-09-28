f4f6d1d92b2a37d02cbda3cb048ae76af13db9c6
Maf1sher <kubas307@gmail.com>
2025-09-28 22:40:15 +0200

    cloud effecr life time

--- scripts/effects/cloud.gd
+++ scripts/effects/cloud.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
 @export var speed: float = 200.0
+@export var lifetime: float = 3
 
 @onready var hitbox = $HitBox
 
 var velocity: Vector2
+
+func _ready() -> void:
+	await get_tree().create_timer(lifetime).timeout
+	queue_free()
 
 func _physics_process(delta: float) -> void:
 	if velocity != Vector2.ZERO:
