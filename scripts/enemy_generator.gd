98e95309f63361974c6d327786c3b24eabe37431
Maf1sher <kubas307@gmail.com>
2025-10-04 20:34:17 +0200

    enemy base class

--- scripts/enemy_generator.gd
+++ scripts/enemy_generator.gd
@@ -3,15 +3,19 @@ extends Node
 
 @export var player: Player
 
-#@onready var orc: PackedScene = preload("res://scenes/entity/Orc.tscn")
-@onready var orc: PackedScene = preload("res://scenes/entity/golem.tscn")
+@onready var orc: PackedScene = preload("res://scenes/entity/Orc.tscn")
+@onready var golem: PackedScene = preload("res://scenes/entity/golem.tscn")
 
 func _ready() -> void:
-	for i in 5:
+	await summon_enemy(3, orc)
+	await summon_enemy(1, golem)
+	
+
+func summon_enemy(amount: int, enemy: PackedScene) -> void:
+	for i in amount:
 		await get_tree().create_timer(2).timeout
-		var orc = orc.instantiate()
-		orc.position = Vector2(randi_range(-80,400),randi_range(-80,400))
-		orc.target = player
-		orc.connect("died", Callable(player, "add_exp"))
-		get_parent().add_child(orc)
-		
+		var new_enemy = enemy.instantiate()
+		new_enemy.position = Vector2(randi_range(-80,400),randi_range(-80,400))
+		new_enemy.target = player
+		new_enemy.connect("died", Callable(player, "add_exp"))
+		get_parent().add_child(new_enemy)
