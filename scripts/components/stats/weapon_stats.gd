e80a38defa04f784bfa194c331f6c75ece882787
Maf1sher <kubas307@gmail.com>
2025-11-24 15:13:41 +0100

    setters for fields in weapon_stats

--- scripts/components/stats/weapon_stats.gd
+++ scripts/components/stats/weapon_stats.gd
@@ -8,8 +8,16 @@ const WEAPON_STAT_NAMES := [
 ]
 
 @export var type: WeaponTypes.Type
-@export var damage: int = 1
-@export var damage_percent: int = 0
+@export var damage: int = 1:
+	set(value):
+		if damage == value: return
+		damage = value
+		emit_signal("stats_changed", "damage", damage)
+@export var damage_percent: int = 0:
+	set(value):
+		if damage_percent == value: return
+		damage_percent = value
+		emit_signal("stats_changed", "damage_percent", damage_percent)
 
 func calculate_damage() -> int:
 	return damage + (damage * damage_percent/100.0)
